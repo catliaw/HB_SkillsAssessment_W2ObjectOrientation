3ea59d3fa8703394409a58f4e1873cf693bab53d
catliaw <cat.liaw@gmail.com>
2016-10-09 21:00:51 -0700

    Adding assessment

---
+++ assessment.py
@@ -0,0 +1,52 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+Encapsulation - Having related data and behavior (methods) bound together.
+
+Abstraction - Being able to use the methods and class structure without having
+to know how it works exactly under the hood.
+
+Polymorphism - Being able to use the same template for data and same methods
+that have different results. For example, two different instances/subclasses
+may use the same structure for data and method, but depending on more specific
+information added to the instance/subclass, the method may have different results.
+
+
+2. What is a class?
+
+A class is a user-defined data structure built upon (under the hood) the dictionary
+data structure, but a class is a much smarter dictionary. Classes are a way to bind
+together related data and functionality (method). Instead of having nested dictionaries
+to organize related data with its label, classes have "instances" or objects which
+can have multiple connected attributes and values. Also, classes can also have related
+functionality/methods.
+
+
+3. What is an instance attribute?
+An instance attribute is the like the a nested dictionary within a dictionary.
+From a class, the user can create an instance or object using the class. With
+that instance or object, the user can create attributes specific to that object.
+
+
+4. What is a method?
+A method is basically the same thing as a function but it specific to the class
+and its members (instances/objects). It is used with the dot method.
+
+
+5. What is an instance in object orientation?
+
+
+
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
